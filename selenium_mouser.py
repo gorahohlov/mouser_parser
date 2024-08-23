75351473058e81a640404f536c0fc0c19c9eca5c
gorahohlov <gorahohlov@gmail.com>
2024-08-23 22:04:05 +0300

    Add two funcs for processing cookie formats and upload cookies to file

--- selenium_mouser.py
+++ selenium_mouser.py
@@ -17,6 +17,7 @@ URL = ('https://www.mouser.com/ProductDetail/ISSI/IS43DR16128B-25EBLI?qs='
        )
 # URL_1 = 'https://www.chipdip.ru/product/ecs-3953m-500-bn-tr-1'
 FILE_NAME = r'c:/users/user/dev/mouser_parse_proj/mouser_cookies.json'
+PATH = r'c:/users/user/dev/mouser_parse_proj/'
 
 
 def save_cookies(file_name='cookies.pkl'):
@@ -29,8 +30,9 @@ def edit_cookies(file_name=None, imported_cookies=None):
     method format"""
     valid_cookies_keys = {'httpOnly', 'value', 'name', 'domain',
                           'secure', 'path', 'expiry', 'sameSite'}
-    valid_sameSite_values = {'no_restriction': 'None', 'unspecified': 'Lax',
+    valid_samesite_values = {'no_restriction': 'None', 'unspecified': 'Lax',
                              'strict': 'Strict'}
+
     if file_name:
         with open(file_name, 'r') as file:
             imported_cookies = json.load(file)
@@ -41,15 +43,48 @@ def edit_cookies(file_name=None, imported_cookies=None):
         cookie = imported_cookies.pop(i)
         if cookie.get('expirationDate'):
             cookie['expiry'] = int(cookie.pop('expirationDate'))
-        cookie = {key: cookie[key] for key in valid_cookies_keys if key in cookie}
-        if valid_sameSite_values.get(cookie['sameSite']):
-            cookie['sameSite'] = valid_sameSite_values[cookie.pop('sameSite')]
+        cookie = {key: cookie[key] for key in valid_cookies_keys
+                  if key in cookie}
+        if valid_samesite_values.get(cookie['sameSite']):
+            cookie['sameSite'] = valid_samesite_values[cookie.pop('sameSite')]
         imported_cookies.insert(i, cookie)
 
     if file_name:
         with open(file_name, 'w') as file:
             json.dump(imported_cookies, file)
+
     return imported_cookies
+
+
+def gen_cookies_file(json_file=None, json_cookies=None):
+    """Generate cookies.txt file in Netscape/Mozilla cookie file format
+    from valid_cookies_keys in json_cookies"""
+#     cookie_format = ['domain', 'flag', 'path', 'secure', 'expiration',
+#                      'name', 'value']
+    if json_file:
+        with open(json_file, 'r') as file:
+            json_cookies = json.load(file)
+    elif not json_cookies:
+        return
+
+    with open(PATH + 'cookies.txt', 'w') as file:
+        file.write('# Netscape HTTP Cookie File')
+        for cookie in json_cookies:
+            if cookie['httpOnly']:
+                line = '#HttpOnly_' + cookie['domain'] + '\t'
+            else:
+                line = cookie['domain'] + '\t'
+            line += 'TRUE\t' if cookie['domain'][0] == '.' else 'FALSE\t'
+            line += cookie['path'] + '\t'
+            line += str(cookie['secure']).upper() + '\t'
+            line += str(int(cookie['expiry'])
+                       ) + '\t' if cookie.get('expiry') else '0\t'
+            line += cookie['name'] + '\t'
+            line += cookie['value'] + '\n'
+#             for i in range(5):
+#                 line += cookie[cookie_format[i]] + '\t'
+#             line += cookie['value'] + '\n'
+            file.write(line)
 
 
 def load_cookies(file_name='cookies.pkl'):
@@ -73,6 +108,7 @@ def load_json_cookies(file_name=FILE_NAME):
 
 if __name__ == '__main__':
 
+    gen_cookies_file(json_file=FILE_NAME)
 #     with open(r'c:/users/user/desktop/mouser_cookies.json', 'r') as file:
 #         cookies = json.load(file)
 #     edit_cookies(imported_cookies=cookies)
